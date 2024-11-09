7cf8ce61452a1b9367ac055d4b81f0e995da1945
GitHubAccount-F <the.blue.man13579@gmail.com>
2024-11-08 23:11:49 -0800

    Delete sec8 directory

--- sec8/lecturedemo/lec16-react-overview-code/register-react/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-
-// Do not use the document.getElementById syntax anywhere else!
-ReactDOM.render(<App></App>, document.getElementById('content'));
